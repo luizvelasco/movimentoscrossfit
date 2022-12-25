db2c4198fbbe2e441ecbc3940c20dbbd200255a8
Luiz Velasco <luizvelasco@gmail.com>
2022-12-25 11:59:40 -0300

    fontes externas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel 4</title>
-        <link rel="stylesheet" href="/css/styles.css">
+@section('title', 'Movimentos do Crossfit')
 
-        <script src="/js/scripts.js"></script>
+@section('content')
 
-       
-    </head>
-    <body class="antialiased">
-       <h1>Teste CSS</h1>
-       <img src="/img/banner.webp" alt="">
-    </body>
-</html>
+<h1>Teste index</h1>
+    
+@endsection
+
