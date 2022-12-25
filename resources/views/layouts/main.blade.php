1cf7c0f2973b030e47749cabfb756c6fccefbe3a
Luiz Velasco <luizvelasco@gmail.com>
2022-12-25 12:21:26 -0300

    adicionando header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,6 +21,26 @@
        
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/logo.webp" alt="Movimentos do Crossfit">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Movimentos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar Movimento</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
         @yield('content')
     <footer>
         <p>Movimentos do Crossfit &copy; 2022</p>
