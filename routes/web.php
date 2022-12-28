a2b301c9fd358233eeff9c77c63802562365a266
Luiz Velasco <luizvelasco@gmail.com>
2022-12-28 18:48:05 -0300

    autenticação

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::get('/', [MovimentController::class, 'index']);
 Route::get('/moviments/create', [MovimentController::class, 'create']);
 Route::get('/moviments/{id}', [MovimentController::class, 'show']);
 Route::post('/moviments', [MovimentController::class, 'store']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
