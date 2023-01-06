bc1898855e1cf3d1ab68369bc28a043e591e294b
Luiz Velasco <luizvelasco@gmail.com>
2023-01-06 18:12:44 -0300

    atualizacao

--- resources/views/moviments/dashboard.blade.php
+++ resources/views/moviments/dashboard.blade.php
@@ -24,11 +24,14 @@
                         <td scope="row">{{ $loop->index + 1 }}</td>
                         <td><a href="/moviments/{{ $moviment->id }}">{{ $moviment->title }}</a></td>
                         <td>
-                            <a href="/moviments/edit/{{ $moviment->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a> 
+                            <a href="/moviments/edit/{{ $moviment->id }}" class="btn btn-outline-warning">Editar</a> 
                             <form action="/moviments/{{ $moviment->id }}" method="POST">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger delete-btn" data-toggle="modal" data-target="#exampleModal"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
+                                {{-- <button type="submit"  class="btn btn-outline-danger" >Deletar</button> --}}
+                                <!-- Botão que irá abrir o modal -->
+                                <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#meuModal">Excluir</button>
+
                             </form>
                         </td>
                     </tr>
@@ -41,5 +44,40 @@
         <p>Não há movimentos cadastrados, <a href="/moviments/create">cadastrar movimento!</a></p>
     @endif
 </div>
+
+<!-- Modal -->
+<div id="meuModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Conteúdo do modal-->
+      <div class="modal-content">
+
+        <!-- Cabeçalho do modal -->
+        <div class="modal-header">
+          <h4 class="modal-title">Excluir movimento</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+
+        <!-- Corpo do modal -->
+        <div class="modal-body">
+          <p>Deseja excluir o movimento?</p>
+        </div>
+
+        <!-- Rodapé do modal-->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger"> Excluir</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 @endsection
--- resources/views/moviments/edit.blade.php
+++ resources/views/moviments/edit.blade.php
@@ -24,15 +24,11 @@
         </div>
         <div class="form-group">
             <label for="title">Descrição: </label>
-            <textarea class="form-control" name="description" id="description" placeholder="Descreva o movimento">{{ $moviment->description }}</textarea>
+            <textarea class="form-control" name="description" id="description" placeholder="Descreva o movimento" >{{ $moviment->description }}</textarea>
         </div>
-        {{-- <div class="form-group">
-            <label for="tags">Tags: </label>
-            <input type="text" class="form-control" id="tags" name="tags" placeholder="Tags de pesquisa">
-        </div> --}}
         <div class="form-group">
             <label for="title">Link: </label>
-            <input type="text" class="form-control" id="link" name="link" placeholder="Link do Movimento" value=" {{ $moviment->link }}">
+            <input type="text" class="form-control" id="link" name="link" placeholder="Link do Movimento" value=" {{ $moviment->link }}" required>
         </div>
         <input type="submit" class="btn btn-primary" value="Editar Movimento">
     </form>
